e9000a33a22879f82718d65eaf82ef0417309c8e
elameen11 <elameen8191.gmail.com>
2023-11-28 08:01:06 -0800

    My commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for num1 in range(10):
-    for num2 in range(10):
-        if num2 > num1 and num1 != 8:
-            print("{}{}".format(num1, num2), end=", ")
-        elif num1 == 8 and num2 == 9:
-            print("{}{}".format(num1, num2))
-            break
-
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i < 8:
+            print("{:d}{:d}".format(i, j), end=", ")
+        else:
+            print("{:d}{:d}".format(i, j))
